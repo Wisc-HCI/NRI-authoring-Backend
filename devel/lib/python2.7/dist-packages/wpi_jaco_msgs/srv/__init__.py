17ed435d19ecf7805b73a141c45fd9f07450e4dc
Jieru Hu <hjr01211@gmail.com>
2018-02-27 11:04:15 -0600

    Add the file to launch backend

--- devel/lib/python2.7/dist-packages/wpi_jaco_msgs/srv/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._EStop import *
-from ._EulerToQuaternion import *
-from ._GetAngularPosition import *
-from ._GetCartesianPosition import *
-from ._JacoFK import *
-from ._QuaternionToEuler import *
