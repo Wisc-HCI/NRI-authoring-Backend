11e107af859685cc14913f5e4d6e64dfc8d47dc3
Jieru Hu <hjr01211@gmail.com>
2018-03-19 20:05:10 -0500

    change launch scripts

---
+++ launch_no_robot.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# make sure gnome-terminal is installed
+# This script launches the backend for therbligs and VREP simulation for the KINOVA MICO arm movement.
+
+echo "Launching backend ROS with no robot connection..." 
+
+# launch the backend, moveit,rviz, fake joint publisher, etc
+gnome-terminal -e "bash -c 'roslaunch mico_planning mico_planning_no_robot.launch;exec $SHELL'"
+sleep 10
+
+# launch the VREP with a preset scene
+gnome-terminal -e "bash -c 'cd ./mico_simulation/V-REP_PRO_EDU_V3_5_0_Linux;./start_vrep.sh;exec $SHELL'"
+sleep 10
+
+# start the back-end with remote API connected to VREP
+gnome-terminal -e "bash -c 'roslaunch mico_planning path_planning.launch sim;exec $SHELL'"
+
+# uncomment the following if want to call Get Position from frontend via rosbridge
+#sleep 5
+# launch the rosbridge server
+#gnome-terminal -e "bash -c 'roslaunch rosbridge_server rosbridge_websocket.launch;exec $SHELL'"
+#sleep 5
+# launch a ROS node to publish end-effector position to front-end
+#gnome-terminal -e "bash -c 'rosrun mico_planning mico_positionpublisher.py;exec $SHELL'"
+
+# create a python client and send some mocked therbligs in json
+gnome-terminal -e "bash -c 'cd ./src/wpi_jaco/mico_planning/src;python mico_client.py;exec $SHELL'"
---
+++ launch_with_robot.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# make sure gnome-terminal is installed
+# This script is to connect to the real robot and launch the entire back-end for KINOVA MICO robot arm.
+
+echo "Launching backend ROS with real robot..." 
+# launch all ROS nodes associated with the KINOVA MICO and connect to the robot
+gnome-terminal -e "bash -c 'roslaunch mico_planning mico_planning_with_robot.launch;exec $SHELL'"
+
+sleep 10
+# start the backend mico_master, litern for the connection from the from end
+gnome-terminal -e "bash -c 'rosrun mico_planning mico_master.py nosim;exec $SHELL'"
+
+# uncomment the following if want to call Get Position from frontend via rosbridge
+#sleep 5
+# launch the rosbridge server
+#gnome-terminal -e "bash -c 'roslaunch rosbridge_server rosbridge_websocket.launch;exec $SHELL'"
+#sleep 5
+# launch a ROS node to publish end-effector position to front-end
+#gnome-terminal -e "bash -c 'rosrun mico_planning mico_positionpublisher.py;exec $SHELL'"
+
+sleep 10
+# create a python client and send some mocked therbligs in json
+gnome-terminal -e "bash -c 'cd ./src/wpi_jaco/mico_planning/src;python mico_client.py;exec $SHELL'"
